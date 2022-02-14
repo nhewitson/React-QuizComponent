7af0d1a660bf824a2f220e7e6f2d71fea51f6c53
Nathan Hewitson <nathan.hewitson@cantarus.com>
2022-02-14 15:40:45 +0000

    displaying quiz questions and handling events

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,6 +10,12 @@ class Quiz extends Component {
     this.state = {quiz_position: 1};
   }
 
+  showNextQuestion() {
+    this.setState(state => {
+      return {quiz_position: state.quiz_position + 1};
+    })
+  }
+
   render() {
     const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
 
@@ -22,7 +28,8 @@ class Quiz extends Component {
     } else {
       return (
         <div>
-          <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+          <QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)}
+          quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
         </div>
       );
     }
