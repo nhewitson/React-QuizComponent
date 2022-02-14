437de677f0e6f9d81da6c31299cbc1bd1242484c
Nathan Hewitson <nathan.hewitson@cantarus.com>
2022-02-14 12:30:39 +0000

    creating a quiz question component 2

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,7 +11,9 @@ class Quiz extends Component {
 
   render() {
     return (
-      <QuizQuestion quiz_question={quizData.quiz_questions[quiz_position - 1]} />
+      <div>
+        <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+      </div>
     );
   }
 }
